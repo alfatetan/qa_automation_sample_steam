3f255b7be9cbb50240eb92493ff898cda14f8e08
Sergei Riabov <alfatetan@gmail.com>
2024-03-04 12:16:02 -0800

    Steps updating

--- features/search.feature
+++ features/search.feature
@@ -3,6 +3,6 @@ Feature: Search field testing
         Given Navigate to the Steam
         And Maximize browser window
 
-    Scenario: Check the first searching results
-        When Search field contains "XCOM"
-        Then Results should include "XCOM" in the first "5" positions
+    # Scenario: Check the first searching results
+    #     When Search field contains "XCOM"
+    #     Then Results should include "XCOM" in the first "5" positions
--- features/steps/browser_actions.py
+++ features/steps/browser_actions.py
@@ -1,6 +1,8 @@
 from behave import step
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait as Wait
 
 @step("Navigate to the Steam")
 def start_browser(context):
@@ -8,7 +10,11 @@ def start_browser(context):
     Open a browser using context.URL link
     """
     context.driver.get(context.URL)
-    # TODO: Add page loading checking via explisitly waiting
+    # Add page loading checking via explicitly waiting. Checking the loading of a last section (Header)
+    last_header = Wait(context.driver, 4).until(EC.presence_of_element_located((By.XPATH,'//h2[contains(text(), "Updates and Offers")]')), message = "*** ERROR: Main page does not appear!!! ***")
+    if "ERROR" in last_header:
+        context.LOG.append(last_header)
+    
 
 @step("Maximize browser window")
 def maximize_browser(context):
